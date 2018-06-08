d8784e2540936ebcb1d280729d235ce888f3b101
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2018-06-08 15:21:09 -0700

    Get express all set up

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Lmao");
+var express = require("express");
+var app = express();
+
+var path = require("path");
+
+app.get("/", function(request, response) {
+  response.sendFile(path.join(__dirname + "/views/index.html"));
+});
+
+app.listen(3000, function() {
+  console.log("Listening on 3000");
+})
